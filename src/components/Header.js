e165532745bcba0b603f42aaa6fd4e1e96153640
Pavel <paha_gagarin@mail.ru>
2022-10-23 15:28:49 +0300

    добавил гитхаб паджес

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,12 +2,12 @@ const Header = () => {
   return (
     <nav className="green darken-1">
       <div className="nav-wrapper">
-        <a className="brand-logo" href="#">
+        <a className="brand-logo" href="/">
           React Shop
         </a>
         <ul id="nav-mobile" className="right hide-on-med-and-down">
           <li>
-            <a href="#">Repo</a>
+            <a href="https://github.com/PavelAratin/reactPracticFortNite" target="_blank">Repo</a>
           </li>
         </ul>
       </div>
